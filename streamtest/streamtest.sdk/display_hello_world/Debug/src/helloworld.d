8241307a363dfb8f3e64c594c82023723d8c214f
sofiamsantos <sofmsantos@gmail.com>
2018-06-04 22:38:12 +0100

    redone streaming - working

---
+++ streamtest/streamtest.sdk/display_hello_world/Debug/src/helloworld.d
@@ -0,0 +1,24 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../display_hello_world_bsp/microblaze_0/include/xil_printf.h \
+ ../../display_hello_world_bsp/microblaze_0/include/xil_types.h \
+ ../../display_hello_world_bsp/microblaze_0/include/xparameters.h \
+ ../../display_hello_world_bsp/microblaze_0/include/mb_interface.h \
+ ../../display_hello_world_bsp/microblaze_0/include/xil_assert.h \
+ ../../display_hello_world_bsp/microblaze_0/include/xil_exception.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../display_hello_world_bsp/microblaze_0/include/xil_printf.h:
+
+../../display_hello_world_bsp/microblaze_0/include/xil_types.h:
+
+../../display_hello_world_bsp/microblaze_0/include/xparameters.h:
+
+../../display_hello_world_bsp/microblaze_0/include/mb_interface.h:
+
+../../display_hello_world_bsp/microblaze_0/include/xil_assert.h:
+
+../../display_hello_world_bsp/microblaze_0/include/xil_exception.h:
